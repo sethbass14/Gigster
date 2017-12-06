b18d1fca77f0a6f911a21071b3e7565a34a2a8c6
github username <github email address>
2017-12-05 21:06:20 -0500

    created a gig. Need to fix the show page

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,2 +1,28 @@
 class GigsController < ApplicationController
+
+  def index
+    @gigs = Gig.all
+  end
+
+  def show
+    @gig = Gig.find(params[:id])
+  end
+
+  def new
+    @gig = Gig.new
+    @users = User.all
+    @cities = City.all
+  end
+
+  def create
+    @gig = Gig.create(gig_params(:name, :location, :city_id, :leader_id, :start_time))
+    redirect_to gig_path(@gig)
+  end
+
+  private
+
+  def gig_params(*args)
+    params.require(:gig).permit(*args)
+  end
+
 end
