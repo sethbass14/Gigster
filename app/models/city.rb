f86f79890c361ebd9c22554548a8a0c564020a2b
github username <github email address>
2017-12-06 11:35:34 -0500

    Added methods to the city model and used them on the City show page

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,9 +2,31 @@ class City < ApplicationRecord
   has_many :musicians, :class_name => "User"
   has_many :gigs
 
-  def city_instruments
-    self.musicians.each do |musician|
-      musician.instruments.select {|instrument| instrument}
-    end
+  def city_musicians_instrument
+    self.musicians.collect {|musician| musician.instruments}.flatten
   end
+
+  def city_instrument_names
+    city_musicians_instrument.collect {|instrument| instrument.name}
+  end
+
+  # Gives us an array of all the the instances of the musicians that play the instruments that are available in the city.
+  # def city_musicians_instance
+  #   musicians = self.city_musicians_instrument.collect {|instrument| instrument.musicians}.compact.flatten
+  #     musicians.select do |musician|
+  #       if musician.city_id == self.id
+  #         musician
+  #       end
+  #     end
+  # end
+
+
+
+  # def city_musicians_name
+  #   city_musicians_instrument.each do |instrument|
+  #     instrument.musicians.each do |musician|
+  #       musician.select {|musician| musician.city == self}
+  #    end
+  #  end
+  # end
 end
