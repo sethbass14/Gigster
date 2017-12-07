5ae031ac2eb7e02dcfc97e1c6dfc290c0985a8fd
github username <github email address>
2017-12-07 09:17:10 -0500

    Fixed the gig edit page and edited the musician show page

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -38,23 +38,10 @@ class GigsController < ApplicationController
 
 
   def update
-
-
     @gig = Gig.find(params[:id])
-
-    # @gig.update(gig_params(:name, :location, :city_id, :leader_id, :start_time))
-    # #turn id to a string. not completely updating musician_ids array.
-    # if params[:gig][:musician_ids].length > 1
-    #   params[:gig][:musician_ids].each do |id|
-    #     if id != ""
-    #       @gig.musician_ids << id.to_i
-    #     end
-    #     byebug
-    #   end
-    # end
-      # @gig.save
-    # # redirect_to :show
-    #
+    @gig.update(gig_params(:name, :location, :city_id, :leader_id, :start_time, musician_ids: []))
+    @gig.save
+    redirect_to gig_path(@gig)
   end
 
   private
