acfc79c171e5c8c6d97a768c30326d3d05124370
github username <github email address>
2017-12-07 08:35:08 -0500

    reset the database with correct seeds

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -42,16 +42,16 @@ class GigsController < ApplicationController
 
     @gig = Gig.find(params[:id])
 
-    @gig.update(gig_params(:name, :location, :city_id, :leader_id, :start_time))
-    #turn id to a string. not completely updating musician_ids array.
-    if params[:gig][:musician_ids].length > 1
-      params[:gig][:musician_ids].each do |id|
-        if id != ""
-          @gig.musician_ids << id.to_i
-        end
-        byebug
-      end
-    end
+    # @gig.update(gig_params(:name, :location, :city_id, :leader_id, :start_time))
+    # #turn id to a string. not completely updating musician_ids array.
+    # if params[:gig][:musician_ids].length > 1
+    #   params[:gig][:musician_ids].each do |id|
+    #     if id != ""
+    #       @gig.musician_ids << id.to_i
+    #     end
+    #     byebug
+    #   end
+    # end
       # @gig.save
     # # redirect_to :show
     #
