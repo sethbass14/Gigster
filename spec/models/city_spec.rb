4d7a8b330429e235fd337220982a0d73b39ca962
github username <github email address>
2017-12-08 13:54:49 -0500

    nav bar added

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,7 +1,15 @@
+require 'rails_helper.rb'
 describe City do
   let(:nyc) { City.create(name: "New York City") }
+  let(:seth) {User.create(first_name: "Seth", last_name: "Barden", age: 31, bio: "A guy", city_id: nyc.id)}
 
   it "has a name " do
     expect(nyc.name).to eq("New York City")
   end
+
+  # it "has many musicians" do
+  #   expect(nyc.musicians).to include(seth)
+  # end
+
+
 end
